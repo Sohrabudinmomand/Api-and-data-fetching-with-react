26acaf7ecd2036f4f290cff8d23262dcc2f497ee
Sohrab momand <sohrabmomand.work@gmail.com>
2023-11-27 11:58:11 +0430

    project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [advice, setAdvices] = useState("")
+
+
+  // We use async and await in the getAdvices function to handle asynchronous operations in a more synchronous-looking way
+  async function getAdvices() {
+    // fetching the data from api
+    const fetchTheApi = await fetch("https://api.adviceslip.com/advice");
+    // changing the data to json
+    const fetchTheApiTojson = await fetchTheApi.json();
+
+    setAdvices(fetchTheApiTojson.slip.advice);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // the parent element starts here
+    <div className='container'>
+      {/* title of the section */}
+      <div className='title-of-the-page'>
+        <h1>Multipurpose application</h1>
+      </div>
+      {/* in the div below i display the data coming from api */}
+      <div className='data-display-box'>
+        <h4 className='info-from-api'>{advice}</h4>
+      </div>
+      {/* a button to change the advice by each click */}
+      <div className='button-box'>
+        <button onClick={getAdvices} id="bottone1"><strong>Discover features</strong></button>
+      </div>
     </div>
-  );
+    // the parent element ends here
+  )
 }
 
 export default App;
+
